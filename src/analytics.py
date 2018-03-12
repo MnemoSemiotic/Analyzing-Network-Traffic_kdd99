61d30b102ce5aa2db17aa4c250c1c50905653d5a
Andrew Roberts <clownfragment@gmail.com>
2018-03-12 17:16:02 -0600

    write function to use bash to create samples from the data set

--- src/analytics.py
+++ src/analytics.py
@@ -1,8 +1,12 @@
+import os
+
 def main():
     pass
 
-    
-def hello():
-    print('Hello')
+def create_random_samples(filepath, n_samples=1000, filename='subsample_0.csv'):
+    '''
+    using coreutils gshuf, create a random sample
+    '''
+    os.system('gshuf -n {} {} > {}'.format(n_samples, filepath, filename))
 
 if __name__ == "__main__": main()
