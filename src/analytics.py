afd70acf800eb5457e5772807aeafb4a0ad0b518
Andrew Roberts <clownfragment@gmail.com>
2018-03-13 08:01:34 -0600

    explore data with various plots

--- src/analytics.py
+++ src/analytics.py
@@ -31,7 +31,7 @@ def plot_hist_basic(df, col):
     """
 
     data = df[col]
-    ax = data.hist(bins=20, normed=1, edgecolor='none', figsize=(10, 7), alpha=.5)
+    ax = data.hist(bins=20, normed=1, edgecolor='none', figsize=(14, 7), alpha=.5)
     ax.set_ylabel('Probability Density')
     ax.set_title(col)
     plt.xticks(rotation=40)
