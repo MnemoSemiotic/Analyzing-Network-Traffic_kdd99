0ac3f96c745602dc1aa965c294d353ca61448192
Andrew Roberts <clownfragment@gmail.com>
2018-03-13 16:02:38 -0600

    add my get_dummies method

--- src/transforms.py
+++ src/transforms.py
@@ -9,17 +9,30 @@ def main():
     pass
 
 def rewrite_values(df, dict_map, column_name):
-    '''
-    INPUT: a pandas dataframe
-           dict mapping column values to new value
-           string with the column name to search/change values in
-    Return: Return a copy of the dataframe with the values changed
-    '''
+
     # here we don't want to alter the original dataframe
     df2 = df.copy()
 
     df2.replace({column_name: dict_map}, inplace=True)
 
     return df2
+
+def my_create_dummies(df, col_list, column_name):
+        '''
+        INPUT:  a pandas dataframe
+               list of column values to be dummy-ized
+               string with the column name to extract dummy values from
+        Return: Return the dataframe with new columns added, all values
+               set to zero except those that correspond to the correct
+               mapping
+        '''
+        df3 = df.copy()
+        for k in col_list:
+            df3[k] = 0
+
+        for k in col_list:
+            df3[k] = np.where(df[column_name] == k, 1, 0)
+
+        return df3
 
 if __name__ == "__main__": main()
