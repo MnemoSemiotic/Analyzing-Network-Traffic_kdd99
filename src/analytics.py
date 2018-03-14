b57b772363bf35f304e9b3411dd2ccdf5c282503
Andrew Roberts <clownfragment@gmail.com>
2018-03-14 12:25:40 -0600

    get list of highly correlated features

--- src/analytics.py
+++ src/analytics.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from collections import defaultdict
+import operator
 
 def main():
     pass
@@ -51,5 +52,20 @@ def count_zeros_message(df):
         if v[1] == len(df):
             message += ('   {} has only zero values\n'.format(k))
     return message
+
+def count_categories(df):
+    '''
+    INPUT: a pandas dataframe
+    Return: dictionary with keys of column names and
+            values of the count of unique items in
+            each column
+    '''
+    subset = df.select_dtypes(include=['object'])
+    cat_dict = {}
+    for column in df.columns:
+        cat_dict[column] = df.groupby(column).count().shape[0]
+    return cat_dict
+
+
 
 if __name__ == "__main__": main()
--- src/transforms.py
+++ src/transforms.py
@@ -41,4 +41,52 @@ def my_create_dummies(df, col_list, col_name):
 
         return df3
 
+def read_data(sample_name):
+    # Create list of column names
+    columns = ['duration',
+           'protocol_type',
+           'service',
+           'flag',
+           'src_bytes',
+           'dst_bytes',
+           'land',
+           'wrong_fragment',
+           'urgent',
+           'hot',
+           'num_failed_logins',
+           'logged_in',
+           'num_compromised',
+           'root_shell',
+           'su_attempted',
+           'num_root',
+           'num_file_creations',
+           'num_shells',
+           'num_access_files',
+           'num_outbound_cmds',
+           'is_host_login',
+           'is_guest_login',
+           'count',
+           'srv_count',
+           'serror_rate',
+           'srv_serror_rate',
+           'rerror_rate',
+           'srv_rerror_rate',
+           'same_srv_rate',
+           'diff_srv_rate',
+           'srv_diff_host_rate',
+           'dst_host_count',
+           'dst_host_srv_count',
+           'dst_host_same_srv_rate',
+           'dst_host_diff_srv_rate',
+           'dst_host_same_src_port_rate',
+           'dst_host_srv_diff_host_rate',
+           'dst_host_serror_rate',
+           'dst_host_srv_serror_rate',
+           'dst_host_rerror_rate',
+           'dst_host_srv_rerror_rate',
+           'label']
+    df = pd.read_csv(sample_name, header=None)
+    df.columns = columns
+    return df
+
 if __name__ == "__main__": main()
